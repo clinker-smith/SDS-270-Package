8bb6a734d8b4eafd52c5479badefb207ecdf4298
tknibb <tknibb@smith.edu>
2025-10-28 15:52:20 -0400

    more help file

--- vg_help_file.R
+++ vg_help_file.R
@@ -1,5 +1,8 @@
-#' Title
-#'
+#' Video Game Genre Printer
+#' 
+#' @description This function will print both the genre and the associated number of 
+#' video games in each genre. You will be able to see which genre is more popular than others.
+#' 
 #' @param ... 
 #'
 #' @return
