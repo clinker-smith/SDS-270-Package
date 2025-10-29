73d909d15b268d1158d9634b3e120ac6ae02e231
tknibb <tknibb@smith.edu>
2025-10-29 13:45:17 -0400

    help file update

--- vg_help_file.R
+++ vg_help_file.R
@@ -3,12 +3,21 @@
 #' @description This function will print both the genre and the associated number of 
 #' video games in each genre. You will be able to see which genre is more popular than others.
 #' 
-#' @param ... 
+#' @details 
+#' 
+#' @param df The data frame that this function 
 #'
-#' @return
-#' @export
+#' @return genre_df 
 #'
 #' @examples
-print_genre <- function(...){
-  print("hello world")
+#' 
+#' @export
+print_genre <- function(df){
+  genre_df <- df |>
+    dplyr::count(genre) |>    
+    dplyr::arrange(desc(n))
+  
+  names(genre_df) <- c("Genre", "Count")
+  result <- print(genre_df)
 }
+
