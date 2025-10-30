d42328f9fea321c42b66d694e209b31ac30ee271
tknibb <tknibb@smith.edu>
2025-10-30 15:55:06 -0400

    final help file

--- vg_help_file.R
+++ vg_help_file.R
@@ -20,10 +20,21 @@
 #' The function returns `NULL` invisibly after printing.
 #'
 #' @examples
+#' vg_data <- read.csv("vgchartz-2024.csv")
+#' print_genre(vg_data)
+#' 
+#' output:
+#'               Genre Count
+#'              Misc  9304
+#'            Action  8557
+#'         Adventure  6260
+#'      Role-Playing  5721
+#'            Sports  5586
+#' ...
 #' 
 #' @export
-print_genre <- function(df){
-  genre_df <- df |>
+print_genre <- function(.data){
+  genre_df <- .data |>
     dplyr::count(genre) |>    
     dplyr::arrange(desc(n))
   
